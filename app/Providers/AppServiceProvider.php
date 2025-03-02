1cc7449bf5989c0cf2ac11ec6df7922c93792523
HosamAlashi1 <as3adel3shi@gmail.com>
2025-03-02 18:44:44 +0200

    cron jobs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Services\DeviceStatusService;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Schema::defaultStringLength(191); // Fix for MySQL older versions
     }
 }
