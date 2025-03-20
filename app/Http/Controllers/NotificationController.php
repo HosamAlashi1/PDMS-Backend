74ff1a09c2dddf9a49e619a1883a8c80a7a50156
HosamAlashi1 <as3adel3shi@gmail.com>
2025-03-20 13:07:33 +0200

    test firebase notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Traits\PushNotification;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class NotificationController extends Controller
